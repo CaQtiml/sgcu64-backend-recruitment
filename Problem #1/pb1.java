d4fd458d958b436d81e7a4392c1c5f80f9851bfa
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2021-06-17 11:01:06 +0700

    Complete: only with right input format

---
+++ Problem #1/pb1.java
@@ -0,0 +1,125 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class pb1 {
+    public static void zero(int i)
+    {
+        if(i==1) System.out.print(" __   ");
+        if(i==2) System.out.print("|  |  ");
+        if(i==3) System.out.print("|__|  ");
+    }
+    public static void one(int i)
+    {
+        if(i==1) System.out.print("      ");
+        if(i==2 || i==3) System.out.print("   |  ");
+    }
+    public static void two(int i)
+    {
+        if(i==1) System.out.print(" __   ");
+        if(i==2) System.out.print(" __|  ");
+        if(i==3) System.out.print("|__   ");
+    }
+    public static void three(int i)
+    {
+        if(i==1) System.out.print(" __   ");
+        if(i==2) System.out.print(" __|  ");
+        if(i==3) System.out.print(" __|  ");
+    }
+    public static void four(int i)
+    {
+        if(i==1) System.out.print("      ");
+        if(i==2) System.out.print("|__|  ");
+        if(i==3) System.out.print("   |  ");
+    }
+    public static void five(int i)
+    {
+        if(i==1) System.out.print(" __   ");
+        if(i==2) System.out.print("|__   ");
+        if(i==3) System.out.print(" __|  ");
+    }
+    public static void six(int i)
+    {
+        if(i==1) System.out.print(" __   ");
+        if(i==2) System.out.print("|__   ");
+        if(i==3) System.out.print("|__|  ");
+    }
+    public static void seven(int i)
+    {
+        if(i==1) System.out.print(" __   ");
+        if(i==2) System.out.print("   |  ");
+        if(i==3) System.out.print("   |  ");
+    }
+    public static void eight(int i)
+    {
+        if(i==1) System.out.print(" __   ");
+        if(i==2) System.out.print("|__|  ");
+        if(i==3) System.out.print("|__|  ");
+    }
+    public static void nine(int i)
+    {
+        if(i==1) System.out.print(" __   ");
+        if(i==2) System.out.print("|__|  ");
+        if(i==3) System.out.print(" __|  ");
+    }
+    public static void colon(int i)
+    {
+        if(i==1) System.out.print("   ");
+        if(i==2) System.out.print(" . ");
+        if(i==3) System.out.print(" . ");
+    }
+    public static void underscore(int i)
+    {
+        if(i==1) System.out.print("    ");
+        if(i==2) System.out.print("    ");
+        if(i==3) System.out.print(" __ ");
+    }
+    public static void error()
+    {
+        for(int j=1;j<=3;j++)
+        {
+            for (int k = 0; k < 8; k++)
+            {
+                if(k==2||k==5) colon(j);
+                else underscore(j);
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        String inpst = kb.nextLine();
+        int len = inpst.length();
+        String[] substr = inpst.split(":",0);
+//        System.out.println(Arrays.toString(substr));
+        if(substr.length!=3) {error();return;}
+        for(int i=1;i<substr.length;i++)
+        {
+            int chk = Integer.parseInt(substr[i]);
+            if(chk<0 || chk>59)
+            {
+                error();
+                return ;
+            }
+        }
+        char[] inp = inpst.toCharArray();
+        for(int j=1;j<=3;j++)
+        {
+            for(int i=0;i<len;i++)
+            {
+                if(inp[i]=='1') one(j);
+                if(inp[i]=='2') two(j);
+                if(inp[i]=='3') three(j);
+                if(inp[i]=='4') four(j);
+                if(inp[i]=='5') five(j);
+                if(inp[i]=='6') six(j);
+                if(inp[i]=='7') seven(j);
+                if(inp[i]=='8') eight(j);
+                if(inp[i]=='9') nine(j);
+                if(inp[i]=='0') zero(j);
+                if(inp[i]==':') colon(j);
+            }
+            System.out.println("");
+        }
+    }
+}
